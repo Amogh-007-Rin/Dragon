34b3eff3af6c418cf0297625950707fc4491c06c
Amogh-007-Rin <amoghdath233@gmail.com>
2025-08-22 16:39:20 +0100

    example of API keys

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("HI MY NAME IS AMOGH")
+let heading = document.querySelector(".heading")
+
+async function getData(){
+    let response = await fetch("https://api.thecatapi.com/v1/images/search?size=med&mime_types=jpg&format=json&has_breeds=true&order=RANDOM&page=0&limit=1")
+    console.log(response.status)
+    heading.innerHTML = response.status
+}
+
+getData()
